437b23cdf747b45a0b43fdee24b9fff232ce708a
MayankT10 <170873949+MayankT10@users.noreply.github.com>
2025-08-28 01:45:23 +0530

    Initial Commit

---
+++ practice-code/Numbers.java
@@ -0,0 +1,15 @@
+public class Numbers {
+	public static void main(String []args) {
+		// This is a single line commment 
+		System.out.println(3);
+		System.out.println(30);
+		System.out.println(10+20);
+		System.out.println(30*10);
+		/* This is multiple line comment 
+		 * line 1 
+		 * line 2 
+		 * line 3
+		 * line 4 */
+		
+	}
+}
